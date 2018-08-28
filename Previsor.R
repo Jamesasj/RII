a091441262ae8022393fd104c2d825ff10aa7c3d
Jamesasj <james@jamesalves.com.br>
2018-08-28 16:23:52 -0300

    prevendo

--- Previsor.R
+++ Previsor.R
@@ -6,16 +6,15 @@ library(stringr)
 library(e1071)
 
 texto <- scan("texto1.txt", what="char", sep="\n", encoding = "UTF-8")
-dados.full <- data_frame(line = 1:length(texto), text = texto)
+dados.full <- data_frame(text = texto)
 dados.bigram <- dados.full %>% unnest_tokens(tregram, text, token = "ngrams", n = 3)
-dados.bigram.summ <- dados.bigram %>% count(tregram, sort = TRUE)
-dados.bigram.summ <- dados.bigram.summ %>% separate(tregram, c("word1","word2","word3"), sep = " ")
-
+dados.bigram.summ <- dados.bigram %>% separate(tregram, c("word1","word2","word3"), sep = " ")
+dados.bigram.summ$n <- NULL
 modelo <- naiveBayes(word3 ~ ., data = dados.bigram.summ)
 
-infe <- data.frame(word1 = "Pátria", word2 = "amada")
+infe <- data.frame(word1 = "Peito", word2 = "a")
+res <- predict(modelo, infe)
+predi <- predict(modelo, infe, type = "raw")
+table(res, dados.bigram.summ$word3)
 
-predict(modelo, infe, type = "class")
-
-
-predict(modelo, infe, type = "raw")
+View(t(res2))
