fae4da2979b2401615df2d8a50f081210d12081d
James Alves <james@jamesalves.com.br>
2018-09-22 15:12:54 -0300

    saida do gráfico

--- T2.GRAFICO/Programa.R
+++ T2.GRAFICO/Programa.R
@@ -23,6 +23,12 @@ system("aLine --similarity --features resultado/cache.txt -o resultMatrix ")
 #unlink(file.name2)
 #unlink(file.name.output)
 
+dados.dicionario <- read.csv("resultado/dictionary.txt",sep = " ", header = FALSE)[1:4]
+dados.similidade <- read.csv("similaridade.txt", sep = " ", header = TRUE)
 
-#dados <- read.csv("resultado/dictionary.txt",sep = " ", header = FALSE)[1:4]
+dados.feature <- read.csv("resultado/features.mtx", sep = " ", header = FALSE)
+dados.dim <- dados.feature[2,]
+dados.feature <- dados.feature[3:nrow(dados.feature),]
+colnames(dados.feature) <- c("documento","feature","qtd")
+
 #colnames(dados)<-c("qtd.letras","palavra","freq","qtd.docs")
