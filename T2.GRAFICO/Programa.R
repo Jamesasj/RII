d43ee9e99c17f4fb08196eaf36924a4d63617c21
James Alves <james@jamesalves.com.br>
2018-09-22 11:59:41 -0300

    adicionado execucação do aLine e criação da pasta de zip

--- T2.GRAFICO/Programa.R
+++ T2.GRAFICO/Programa.R
@@ -1,18 +1,24 @@
 file.name <- "https://inf.ufes.br/~elias/dataSets/basic-datasets.tar.gz"
+file.context <- dirname(rstudioapi::getSourceEditorContext()$path)
 file.name2 <- "dataset.tar.gz"
-mainDir = getwd()
-subDir <- "output"
 
-dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
-setwd(file.path(mainDir, subDir))
+setwd(file.context)
+
+file.folder.mainDir = getwd()
+file.folder.output <- "output"
+
+
+dir.create(file.path(file.folder.mainDir, file.folder.output), showWarnings = FALSE)
 
 download.file(file.name, destfile =  file.name2)
-
-file.list <- untar(file.name2, list = TRUE)
-untar(file.name2)
+untar(file.name2, exdir = file.path(file.folder.mainDir, file.folder.output))
 
 
+file.list <- data.frame(arquivos = list.files(file.folder.output, full.names = TRUE))
+write.table(file.list,"indice.dat", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+retorn <- system("aLine -i -l indice.dat -d resultado")
 
 
-setwd(file.path(mainDir))
-unlink(subDir, recursive = TRUE)
+#setwd(file.path(file.folder.mainDir))
+#unlink(subDir, recursive = TRUE)
