d99679201a4371b7b4145d87c8b03c44803e108e
Jamesasj <james@jamesalves.com.br>
2018-09-27 16:43:52 -0300

    adicionado controle de tempo de acesso a pagina

--- T8.NAVEGADOR/Programa.py
+++ T8.NAVEGADOR/Programa.py
@@ -1,13 +1,12 @@
 
 from flask import Flask, redirect, url_for, request,render_template, abort,jsonify
-import os, json
+import os, json, csv
 
 app = Flask("CodeFlix")
 
 @app.route('/')
 def home():
     return  render_template('index.html')
-
 
 @app.route('/pagina1')
 def pagina1():
@@ -16,6 +15,16 @@ def pagina1():
 @app.route('/pagina2')
 def pagina2():
     return  render_template('/pagina2.html')
+
+@app.route('/gravar', methods=['POST'])
+def gravar():
+    pagina = request.form['pagina']
+    entrada = request.form['entrada']
+    saida = request.form['saida']
+    tempo = request.form['tempo']
+    with open("resultados.csv",'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([pagina, entrada, saida, tempo ])
 
 if __name__=='__main__':    
     app.run('0.0.0.0',debug=True, port=8080)
