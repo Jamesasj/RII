080757cd9d76177b6a6368a155b5473e510b90f2
Jamesasj <james@jamesalves.com.br>
2018-09-28 14:35:25 -0300

    adcionado execucao da amostra

---
+++ T12.SALTON/Programa.R
@@ -0,0 +1,12 @@
+setwd("/home/james/Documents/RII/T12.SALTON")
+dataset.link <- "https://inf.ufes.br/~elias/dataSets/aTribuna-21dir.tar.gz"
+folders.raiz <- getwd()
+
+download.file(dataset.link, "dataset.tar.gz")
+untar("dataset.tar.gz", exdir = file.path(folders.raiz, "output"))
+arquivos.lista <- list.files("output", ".txt", recursive = T, full.names = T)
+arquivos.amostra <- data.frame(arquivos = sample(arquivos.lista, 200))
+write.table(arquivos.amostra, "indice.dat", quote = F,row.names =  F, col.names = F)
+system("aLine -i -l indice.dat -d output2")
+system("aLine --convert -d output2")
+
