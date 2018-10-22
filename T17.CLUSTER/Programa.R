28305a6f39af32f0daf70019d53b22d74cd82563
James Alves <james@jamesalves.com.br>
2018-10-22 17:09:24 -0300

    adicionado comando

--- T17.CLUSTER/Programa.R
+++ T17.CLUSTER/Programa.R
@@ -1,5 +1,12 @@
-setwd("C:\\Users\\james\\PROJETOS\\RII\\T17.CLUSTER")
+#setwd("C:\\Users\\james\\PROJETOS\\RII\\T17.CLUSTER")
 
 dados <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header = FALSE)
 
-write.csv(dados, sep = ";", "/iris/iris.data")
+write.csv(dados, sep = ";", "iris.data")
+
+write.table(dados[1:4], sep = " ", "iris_data.mtx", 
+          row.names = FALSE, col.names = FALSE)
+
+commando = 'aLine --clustering --algorithm kmeans --features iris_data.mtx -k 3 --num-inter 200'
+
+system(commando)
