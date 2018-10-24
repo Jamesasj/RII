503e68d10118f1e749597f1b9d61ed25426576fb
James Alves <james@jamesalves.com.br>
2018-10-24 16:02:51 -0300

    corrigido saida de feature

--- T18.CLUSTER/Programa.R
+++ T18.CLUSTER/Programa.R
@@ -7,8 +7,14 @@ dataset <- read.csv2("marcoCivil.csv")
 library(tidytext)
 library(dplyr)
 
-dados.twitter <- data.frame(id = 1:nrow(dataset), text = as.character(dataset$text))
+dados.twitter <- data.frame(id = 1:nrow(dataset), 
+                            text = as.character(dataset$text))
+
 dados.twitter$text <- as.character(dados.twitter$text)
+
+dados.classe = data.frame(id = dados.twitter$id, 
+                          classe = dataset$Sentido)
+
 dados.df <- dados.twitter %>% unnest_tokens(word, text)
 dados.fw <- dados.df %>% count(word, sort = T)
 dados.fw$id <- c(1:nrow(dados.fw))
@@ -19,4 +25,15 @@ dados$word <- NULL
 colnames(dados) <- c("twitt", "palavra")
 dados2 <- dados %>% count(twitt, palavra)
 
-write.table(dados2, "saida.csv")
+nDocs <- nrow(dataset)
+nFeatu <- nrow(dados.fw)
+totalF <- sum(dados.fw$n)
+
+lista <- c(nDocs, nFeatu, totalF)
+dados2 <- rbind(lista, dados2)
+cat("%%FEATURES.MTX\n", file= "features.mtx", append = F )
+write.table(dados2, "features.mtx", append = T, row.names = F, col.names = F)
+
+write.table(dados.classe, "classe.csv", row.names = F)
+write.table(dados.fw, "dicionario.csv", row.names = F)
+
