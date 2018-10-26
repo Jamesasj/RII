219421f4a9c986908501294e79bb86425dcbae00
James Alves <james@jamesalves.com.br>
2018-10-25 22:13:55 -0300

    ajustado k

--- T18.CLUSTER/Programa2.R
+++ T18.CLUSTER/Programa2.R
@@ -3,8 +3,8 @@
 # (OLIVEIRA et al., 2014). Para este experimento utilize obrigatoriamente as
 # bases do Marco Civil-{I e II}. Escolha uma base extra, presente no artigo
 # ou da literatura. Discuta, detalhe e proponha uma melhoria no algoritmo
-# da Figura 1, descrito em (OLIVEIRA et al., 2014). Submeta os códigos
-# necessários para estes experimentos.
+# da Figura 1, descrito em (OLIVEIRA et al., 2014). Submeta os cï¿½digos
+# necessï¿½rios para estes experimentos.
 
 #setwd("C:/Users/james/PROJETOS/RII/T18.CLUSTER/")
 
@@ -17,4 +17,7 @@ write.table(dataset.classe1.teste, file = "treino.txt", col.names =F, row.names 
 dataset.classe2.teste = data.frame(id = c(4300:nrow(dataset.matrix)))
 write.table(dataset.classe1.teste, file = "teste.txt", col.names =F, row.names = F, append = F)
 
-system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc1.class -k 5 -o output.txt")
+system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc1.class -k 3 -o output1.txt")
+
+
+system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc2.class -k 13 -o output2.txt")
