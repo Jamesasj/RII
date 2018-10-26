96a8c9010b14f37bce1304d9efc0b004e5901482
James Alves <james@jamesalves.com.br>
2018-10-25 23:51:45 -0300

    ajuste de pastas

--- T18.CLUSTER/Programa2.R
+++ T18.CLUSTER/Programa2.R
@@ -5,12 +5,9 @@
 # ou da literatura. Discuta, detalhe e proponha uma melhoria no algoritmo
 # da Figura 1, descrito em (OLIVEIRA et al., 2014). Submeta os c�digos
 # necess�rios para estes experimentos.
+# setwd("C:/Users/james/PROJETOS/RII/T18.CLUSTER/")
 
-#setwd("C:/Users/james/PROJETOS/RII/T18.CLUSTER/")
-
-
-dataset.matrix <- read.csv2("marcoCivil/marcocivil.mtx", sep = " ", header = F,skip = 2)[1:3]
-
+dataset.matrix <- read.csv2("marcocivil.mtx", sep = " ", header = F,skip = 2)[1:3]
 
 dataset.classe1.teste = data.frame(id = c(0:4300))
 write.table(dataset.classe1.teste, file = "treino.txt", col.names =F, row.names = F, append = F)
@@ -18,7 +15,5 @@ write.table(dataset.classe1.teste, file = "treino.txt", col.names =F, row.names 
 dataset.classe2.teste = data.frame(id = c(4300:max(dataset.matrix$V1)))
 write.table(dataset.classe2.teste, file = "teste.txt", col.names =F, row.names = F, append = F)
 
-system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc1.class -k 3 -o output1.txt")
-
-
-system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc2.class -k 12 -o output2.txt")
+system("aLine --classifier --algorithm knn --features marcocivil.mtx --train treino.txt --test teste.txt --labels mc1.class -k 3 -o output1.txt")
+system("aLine --classifier --algorithm knn --features marcocivil.mtx --train treino.txt --test teste.txt --labels mc2.class -k 12 -o output2.txt")
