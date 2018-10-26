860a55bb1e57646a9f190a7b0afc875ef2710e84
James Alves <james@jamesalves.com.br>
2018-10-25 21:55:56 -0300

    clear code

--- T18.CLUSTER/Programa2.R
+++ T18.CLUSTER/Programa2.R
@@ -9,36 +9,15 @@ library(dplyr)
 # da Figura 1, descrito em (OLIVEIRA et al., 2014). Submeta os códigos
 # necessários para estes experimentos.
 
-setwd("C:/Users/james/PROJETOS/RII/T18.CLUSTER/")
+#setwd("C:/Users/james/PROJETOS/RII/T18.CLUSTER/")
 
 
-dataset.matrix <- read.csv2("marcoCivil/marcocivil.mtx", 
-                            sep = " ", header = F, 
-                            skip = 2)[1:3]
+dataset.matrix <- read.csv2("marcoCivil/marcocivil.mtx", sep = " ", header = F,skip = 2)[1:3]
 
-dataset.classe1 <- read.csv2("marcoCivil/mc1.class", 
-                             header = F,  skip = 1)[1]
+dataset.classe1.teste = data.frame(id = c(0:4300))
+write.table(dataset.classe1.teste, file = "treino.txt", col.names =F, row.names = F, append = F)
 
-dataset.classe2 <- read.csv2("marcoCivil/mc2.class", 
-                             header = F, skip = 1)[1]
+dataset.classe2.teste = data.frame(id = c(4300:nrow(dataset.matrix)))
+write.table(dataset.classe1.teste, file = "teste.txt", col.names =F, row.names = F, append = F)
 
-dataset.matrix.test <- dataset.matrix %>% filter(V1 > 4300 )
-dataset.classe1.teste <- data.frame( classes = dataset.classe1[-1:-4299, ])
-dataset.classe2.teste <- data.frame( classes = dataset.classe2[-1:-4299, ])
-
-reg <- nrow(dataset.classe1.teste)
-featu <- max(dataset.matrix.test$V2)
-tam <- nrow(dataset.matrix.test)
-
-matrixMarket.test <- rbind(c(reg, featu, tam), dataset.matrix.test)
-
-cat("%%MatrixMarket matrix coordinate integer general", file = "matrix.mtx", append = F)
-write.table(matrixMarket.test, file = "teste.txt", col.names = F, row.names = F, append = T)
-
-cat("# Posicao politica", file = "classes1.txt", append = F)
-write.table(dataset.classe1.teste, file = "classes1.txt", col.names = F, row.names = F, append = T)
-
-cat("# Posicao politica", file = "classes2.txt", append = F)
-write.table(dataset.classe2.teste, file = "classes2.txt", col.names = F, row.names = F, append = T)
-
-
+system("aLine --classifier --algorithm knn --features marcoCivil/marcocivil.mtx --train treino.txt --test teste.txt --labels marcoCivil/mc1.class -k 5 -o output.txt")
