beff828d00c5633cfab07622e58ad1d834be6174
James Alves <james@jamesalves.com.br>
2018-12-18 16:18:26 -0200

    alterado para que modelo seja salvo

--- T22.ZIPFUziel/Programa.R
+++ T22.ZIPFUziel/Programa.R
@@ -1,46 +1,35 @@
 library(SnowballC)
 library(lsa)
 library(textreadr)
-
-setwd("c://Users/james/PROJETOS/RII/T22.ZIPFUziel/")
-
-tempDire = tempfile()
-dir.create(tempDire)
-
-tempDire.data.tar <- paste(tempDire,"usielCarneiro.tar.gz", sep = "/" )
-download.file("https://inf.ufes.br/~elias/dataSets/usielCarneiro.tar.gz", destfile = tempDire.data.tar)
-
-untar(tempDire.data.tar, exdir= tempDire)
-
-lista.files <- list.files(tempDire, all.files=T, full.names = T, include.dirs = T, 
-                    recursive = T, pattern = "docx")
-
-lista.files.names <- list.files(tempDire, all.files=T, full.names = F, include.dirs = F, 
-                          recursive = T, pattern = "docx")
-
-write.table(lista.files.names, "saida.txt",row.names = F, col.names = F)
-library(rtika) #rtika::install_tika()
-tika_text(lista.files)
-
-for (ix in 1:length(lista.files)) {
-  try({
-    txt = read_docx(lista.files[ix])
-    nomeArquivo = paste("output", paste(ix,".txt", sep = ""), sep = "/")
-    write( txt, file=nomeArquivo)
-  })
-}
-
 library(tm)
-arquivos.convertidos <- list.files("output",  full.names = T)
-
-data.corpus <- VCorpus(DirSource("output"))
-matrix.dtm <- DocumentTermMatrix(matrix)
-
 library(dplyr)
 library(tidytext)
 library(tidyr)
 
-dados.td <- tidy(data.corpus)
+#setwd("c://Users/james/PROJETOS/RII/T25.ZIPFAtribuna/")
+
+tempDire = tempfile()
+dir.create(tempDire)
+
+tempDire.data.tar <- paste(tempDire,"dadosbaixados.tar.gz", sep = "/" )
+download.file("http://www.inf.ufes.br/???elias/dataSets/aTribuna-21dir.tar.gz", destfile = tempDire.data.tar)
+
+untar(tempDire.data.tar, exdir= tempDire)
+
+lista.files <- list.files(tempDire, all.files=T, full.names = T, include.dirs = T, 
+                    recursive = T, pattern = "txt")
+
+lista.files.names <- list.files(tempDire, all.files=T, full.names = F, include.dirs = F, 
+                          recursive = T, pattern = "txt")
+
+write.table(lista.files.names, "saida.txt",row.names = F, col.names = F)
+
+data.corpus <- VCorpus(DirSource(tempDire, recursive = T, pattern = "txt"))
+
+dados.td.all <- tidy(data.corpus)
+rm(data.corpus)
+
+dados.td <- sample_frac(dados.td.all, .025)
 
 dados.token <- dados.td %>% 
   unnest_tokens(word, text) %>% 
@@ -57,9 +46,16 @@ dados.matrix <- dados.matrix[order(-dados.matrix$TF),]
 dados.matrix$rank <- 1:nrow(dados.matrix)
 linha <- lm(dados.matrix$TF ~ dados.matrix$rank)
 
+jpeg("zipf_resulta.jpg")
 plot(dados.matrix$TF ~ dados.matrix$rank , 
-     ylab = "Frequencia de Termos (log2)",
+     ylab = "Frequencia em documentos (log2)",
      xlab = "Rank de Termos",
      col = "blue", pch = 19, cex = 1, lty = "solid", lwd = 2)
-abline(linha, lwd=2, col="red")
+abline(linha, lwd = 2, col="red")
+dev.off()
 
+rm(dados.td, dados.matrix, dados.token, linha, lista.files, 
+   lista.files.names, tempDire, tempDire.data.tar)
+
+rm(tempDire)
+save(dados.td.all, file = "data.Rdata")
